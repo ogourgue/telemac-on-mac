6139067634c321420c55ba6bd0207e1d8f67e3f8
Olivier Gourgue <ogourgue@gmail.com>
2020-05-26 18:35:09 -0400

    change test case in version 8.0.2
    bosse_vase (like other versions) instead of bosse-t2d (choosen by mistake)

--- v8p0r2/bosse_vase/bosse_vase.sh
+++ v8p0r2/bosse_vase/bosse_vase.sh
@@ -5,4 +5,4 @@ export HOMETELONMAC=$HOME/Applications/telemac/telemac-on-mac/v8p0r2
 source $HOMETELONMAC/configs/pysource.mp-gcc9.sh
 
 # run telemac2d
-telemac2d.py t2d_bosse-t2d.cas
+telemac2d.py t2d_bosse_vase-t2d.cas
--- v8p0r2/bosse_vase/bosse_vase_parallel.sh
+++ v8p0r2/bosse_vase/bosse_vase_parallel.sh
@@ -5,4 +5,4 @@ export HOMETELONMAC=$HOME/Applications/telemac/telemac-on-mac/v8p0r2
 source $HOMETELONMAC/configs/pysource.mp-openmpi-gcc9.sh
 
 # run telemac2d
-telemac2d.py t2d_bosse-t2d.cas --ncsize=4
+telemac2d.py t2d_bosse_vase-t2d.cas --ncsize=4
